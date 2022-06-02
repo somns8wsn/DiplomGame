c1cf75f9fc281fe49c68ada3744a96e72381e268
Yulpan <74107125+Yulpan@users.noreply.github.com>
2022-06-02 14:00:42 +0300

    02.06.2022
    Implemented TV, qte-window and animation

--- Scripts/home_01.gd
+++ Scripts/home_01.gd
@@ -1,6 +1,7 @@
 extends Node2D
 
 var floor_junk := preload("res://Scenes/floor_junk.tscn")
+onready var TV = load("TV")
 
 func _ready() -> void:
 	$slippers/Sprite.set_flip_h(true)
@@ -16,3 +17,8 @@ func _ready() -> void:
 func _physics_process(delta: float) -> void:
 	#$dumbbell.position.y = 578.346
 	pass
+
+
+func _on_Area2D_area_entered(area):
+	if entered.visibility_changed("TV"):
+		$TV.visible = !$TV.visible
--- Scripts/tv.gd
+++ Scripts/tv.gd
@@ -1,5 +1,2 @@
 extends Node2D
 
-
-var distance2Hero = enemy.get_global_pos().distance_to(hero.get_global_pos()); 
-	if(distance2Hero<100): enemy.throwBulletAt(hero);
