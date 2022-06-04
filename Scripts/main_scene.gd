1a4283bfd259274564f8e3b6ed577ce7aa55a24d
somns8wsn <tvsp6q@bk.ru>
2022-06-04 03:50:53 +0300

    repaired

--- Scripts/main_scene.gd
+++ Scripts/main_scene.gd
@@ -1,8 +1,6 @@
 extends Node
 
-var player_pos = $topdown_player.get_position()
-var obj_pos = $Sprite.get_position()
-var distance = player_pos.distance_to(obj_pos)
+
 export var fade_distance = 1500
 export var max_opacity = 1
 export var min_opacity = 0.2
@@ -21,10 +19,13 @@ func _ready() -> void:
 
 
 func _process(_delta) -> void:
-#	print($topdown_player.get_position())
 	pass
 
 func _physics_process(_delta) -> void:
+	var player_pos = $topdown_player.get_position()
+	var obj_pos = $Sprite.get_position()
+	var distance = player_pos.distance_to(obj_pos)
+
 	$Sprite.modulate = Color(1,1,1,
 							clamp(1 - ( ( (distance)/fade_distance) - buffer ),
 							min_opacity,
